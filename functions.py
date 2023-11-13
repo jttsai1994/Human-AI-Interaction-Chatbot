19cecdc7d7b24e49985674bc24d080d6d4d947f4
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-11-13 23:57:06 +0000

    move rules to params.py

--- functions.py
+++ functions.py
@@ -1,25 +1,10 @@
 import re
 import random
 
-rules = {
-'hello|hi':["Nice to meet you! What can I help you?","Hi, are you alright?","Hello, good to see you again!"],
-'I want (.*)': 
-                ['What would it mean if you got {0}',
-                'Why do you want {0}',
-                "What's stopping you from getting {0}"],
- 'do you remember (.*)':
-                        ['Did you think I would forget {0}',
-                        "Why haven't you been able to forget {0}",
-                        'What about {0}',
-                        'Yes .. and?'],
- 'do you think (.*)': 
-                    ['if {0}? Absolutely.', 'No chance'],
- 'if (.*)': 
-                ["Do you really think it's likely that {0}",
-                'Do you wish that {0}',
-                'What do you think about {0}',
-                'Really--if {0}']
-  }
+import params
+
+rules = params.rules
+
 # Define match_rule()
 def match_rule(rules, message):
     response, phrase = "default", None
