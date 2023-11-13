33efd1559bf4ea30cb1c1983357b9f9621024fa2
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-11-13 23:57:25 +0000

    create params.py to store all params

---
+++ params.py
@@ -0,0 +1,34 @@
+import re
+
+rules = {
+'hello|hi':["Nice to meet you! What can I help you?","Hi, are you alright?","Hello, good to see you again!"],
+'I want (.*)': 
+                ['What would it mean if you got {0}',
+                'Why do you want {0}',
+                "What's stopping you from getting {0}"],
+ 'do you remember (.*)':
+                        ['Did you think I would forget {0}',
+                        "Why haven't you been able to forget {0}",
+                        'What about {0}',
+                        'Yes .. and?'],
+ 'do you think (.*)': 
+                    ['if {0}? Absolutely.', 'No chance'],
+ 'if (.*)': 
+                ["Do you really think it's likely that {0}",
+                'Do you wish that {0}',
+                'What do you think about {0}',
+                'Really--if {0}']
+  }
+
+keywords = {
+    'goodbye': ['bye', 'farewell'],
+    'greet': ['hello', 'hi', 'hey'],
+    'thankyou': ['thank', 'thx']}
+
+
+
+patterns = {}
+
+for intent, keys in keywords.items():
+    # Create regular expressions and compile them into pattern objects
+    patterns[intent] = re.compile('|'.join(keys))
