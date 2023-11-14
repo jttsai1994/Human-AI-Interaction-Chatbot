dd760b02b3abb8e1508c7382cbd290c1299f36be
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-11-14 00:18:29 +0000

    add respond() and responses params

--- find_intent.py
+++ find_intent.py
@@ -1,7 +1,10 @@
 import re
+import random
 
 import params
+
 patterns = params.patterns
+responses = params.response_for_intent
 def match_intent(message):
     matched_intent = None
     for intent, pattern in patterns.items():
@@ -9,3 +12,13 @@ def match_intent(message):
         if re.search(pattern,message):
             matched_intent = intent
     return matched_intent
+
+def respond(message):
+    # Call the match_intent function
+    intent = match_intent(message)
+    # Fall back to the default response
+    key = "default"
+    if intent in responses:
+        key = intent
+    response = random.choice(responses[key])
+    return response
