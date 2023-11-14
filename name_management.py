521a344e4c8b183eec853f01d4e97f7e84594bac
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-11-14 23:49:43 +0000

    change the condition of asking or reply name
    if intent = initiate therefore from_name_management = false
    
    if intent = name_management but bot don't know userName, then askName

--- name_management.py
+++ name_management.py
@@ -1,7 +1,7 @@
 import random
-def askName(userName,firstTime=1):
+def askName(userName,from_name_managment=0):
     if not userName:
-        if firstTime:
+        if not from_name_managment:
             userName = input("Hi! Please tell me your name: ")
         else:
             userName = input("Sorry I don't know your name yet, please tell me: ")
@@ -14,7 +14,7 @@ def replyName(userName):
              f"Nice to hear from you again! {userName}",
              f"Everyone knows you are {userName}"]
     if not userName: # if user has not been asked for Name
-        askName(0)
+        askName(userName,from_name_managment=1)
     else:
         print (random.choice(reply))
         return userName
