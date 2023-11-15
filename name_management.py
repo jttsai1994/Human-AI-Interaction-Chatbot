02776f6beb96e969d3986e6797f059f1463d5f65
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-11-15 00:31:06 +0000

    replyName sould return in first if condition

--- name_management.py
+++ name_management.py
@@ -3,10 +3,9 @@ def askName(userName,from_name_managment=0):
     if not userName:
         if not from_name_managment:
             userName = input("Hi! Please tell me your name: ")
-            return userName
         else:
             userName = input("Sorry I don't know your name yet, please tell me: ")
-            return userName
+        return userName
     else: #if bot already know userName but the user's intent as "initiate"
         replyName(userName)
     
@@ -15,7 +14,7 @@ def replyName(userName):
              f"Nice to hear from you again! {userName}",
              f"Everyone knows you are {userName}"]
     if not userName: # if user has not been asked for Name
-        askName(userName,from_name_managment=1)
+        return askName(userName,from_name_managment=1)
     else:
         print (random.choice(reply))
         return userName
