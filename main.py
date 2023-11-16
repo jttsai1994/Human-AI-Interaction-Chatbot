7be74f6c34281a68ce6fb130524a0d6e3f2de1dc
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-11-16 00:02:16 +0000

    add random choice to default and discoverability
    small talk also need to response QA

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import creating_QA_model
 # import match_rules
 # import find_intent
 # import qa_matching
+import params
 import name_management
 from joblib import load
 import re
@@ -13,8 +14,8 @@ def main():
     stop = False
     qn_q= load('Qid_Q_dict')   #load params:qn_q
     q_a = load('QA_Dictionary') #load params:q_a
+    replies = params.responses_usual
     while not stop:
-        print(f"userName is {userName}")
         user_input = input(" (type anything to ask me, or enter 'stop' to quit) :")
         if user_input.lower() == 'stop':
             print("Goodbye!")
@@ -23,17 +24,19 @@ def main():
         else:
             # response = find_intent.respond(user_input)          
             _intent = creating_QA_model.answer(user_input)
-            print(f"return intent from creating_QA_model.answer : {_intent}")   #for debugging
+            # print(f"return intent from creating_QA_model.answer : {_intent}")   #for debugging
             if _intent =="initiate":
                 userName = name_management.askName(userName)
-                response = f"Nice to meet you! {userName}"
+                response = random.choice(replies['greet'])
             elif _intent =="identity management":
                 userName = name_management.replyName(userName)
                 response = f"{userName}, What can I do for you"
-            elif re.search('Q*',_intent)[0] :  #response answer
+            elif re.search('small*|Q*',_intent)[0] :  #response answer for Question and small talk
                 response = random.choice(q_a[qn_q[_intent]])
-            elif _intent == 'defualt': #defualt reply
-                response = "Sorry I can not understand. Can you give me more details?"
+            elif _intent =="discoverability":
+                response = random.choice(replies[_intent])          
+            elif _intent == 'default': #defualt reply
+                response = random.choice(replies[_intent])
             print(response)
 
 
