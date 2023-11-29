4cbab87ee9728be04a5a9f9667927fab39acbe13
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-11-29 22:24:36 +0000

    Create User class object

---
+++ user.py
@@ -0,0 +1,17 @@
+class User:
+    def __init__(self):
+        self.name = None
+        self.confirm = False
+        self.movie = None
+        self.visitDate = None
+        self.payMethod = None
+        self.pay = None
+
+    def set_name(self,name):
+        self.name = name
+    def target_movie(self,movie):
+        self.movie = movie
+    def visit_at(self,date):
+        self.visitDate = date
+    def pay_by(self,method):
+        self.payMethod = method
