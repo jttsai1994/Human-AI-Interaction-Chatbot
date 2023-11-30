936a4c6c28414acab02f6e84e4e38c1d40b2814f
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-11-30 22:39:02 +0000

    if the user input a wrong time index, the loop won't stop

--- choose_movie_time.py
+++ choose_movie_time.py
@@ -6,17 +6,18 @@ def decide_date(user):
     days_mapping = {1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday", 7: "Sunday"}
     days_list = [days_mapping[index] for index in movie_date]
     days_pattern = '|'.join(list(days_mapping.values())).lower()
-    if not user.visitDate:
-        print(f"Seems like you haven't choose the Date to watch {user.movie}")
-        user_ans= input(f" We can watch {user.movie} on:{days_list}, which day would you like?: ")
-        if re.search(days_pattern,user_ans.lower()):
-            user_date = re.search(days_pattern,user_ans.lower())[0]
-            user.visit_at(user_date.upper())
-            print(f"OK, looking forward to see you on {user.visitDate}")
+    while user.visitDate:
+        if not user.visitDate:
+            print(f"Seems like you haven't choose the Date to watch {user.movie}")
+            user_ans= input(f" We can watch {user.movie} on:{days_list}, which day would you like?: ")
+            if re.search(days_pattern,user_ans.lower()):
+                user_date = re.search(days_pattern,user_ans.lower())[0]
+                user.visit_at(user_date.upper())
+                print(f"OK, looking forward to see you on {user.visitDate}")
+            else:
+                print(f"Sorry, we can't arrange with your answer.")
         else:
-            print(f"Sorry, we can't arrange with your answer.")
-    else:
-        pass
+            pass
 
 
 
