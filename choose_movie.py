93e84020497e0757546b7102533f0f8cd8e2c948
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-11-30 22:38:16 +0000

    add a leave variable to let user leave the booking process if the user has not decided

--- choose_movie.py
+++ choose_movie.py
@@ -5,15 +5,22 @@ from params import responses_movie_list
 import params
 def ask_which_movie(user):
     movie_pattern = "|".join(movie_list).lower()
-    if not user.movie:
-        print(random.choice(responses_movie_list))
-        print("Seems like you haven't choose a movie to watch")
-        user_ans= input("What do you want to watch? :")
-        if re.search(movie_pattern,user_ans.lower()):
-            user_movie = re.search(movie_pattern,user_ans.lower())[0]
-            user.target_movie(user_movie)
-            print(f"OK, we will choose {user_movie} for you")
+    leave = False
+    while not (user.movie or leave):
+        if not user.movie:
+            print(random.choice(responses_movie_list))
+            print("Seems like you haven't choose a movie to watch")
+            user_ans= input("What do you want to watch? :")
+            if re.search(movie_pattern,user_ans.lower()):
+                user_movie = re.search(movie_pattern,user_ans.lower())[0]
+                user.target_movie(user_movie)
+                print(f"OK, we will choose {user_movie} for you")
+            else:
+                print(f"Sorry, we can't arrange with your answer.")
+                decide_yet = input("Have you decided which movie? (Y/N): ")
+                if 'n' in decide_yet.lower():
+                    leave = True
         else:
-            print(f"Sorry, we can't arrange with your answer.")
-    else:
-        print(f"Seems like you are interested in watching {user.movie}")
+            print(f"Seems like you are interested in watching {user.movie}")
+    
+    return (user.movie or leave)
