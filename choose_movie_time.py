e3c8ac86dd858cf5213fd605d2c2548aa6c39d59
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-11-30 23:00:35 +0000

    bug fixed for while loop condition

--- choose_movie_time.py
+++ choose_movie_time.py
@@ -6,7 +6,7 @@ def decide_date(user):
     days_mapping = {1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday", 7: "Sunday"}
     days_list = [days_mapping[index] for index in movie_date]
     days_pattern = '|'.join(list(days_mapping.values())).lower()
-    while user.visitDate:
+    while not user.visitDate:
         if not user.visitDate:
             print(f"Seems like you haven't choose the Date to watch {user.movie}")
             user_ans= input(f" We can watch {user.movie} on:{days_list}, which day would you like?: ")
