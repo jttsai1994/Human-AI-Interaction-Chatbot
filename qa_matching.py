0ae827cecbe727476f717278c8f6419ad0b39ac1
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-11-30 00:57:34 +0000

    delete useless code

--- qa_matching.py
+++ qa_matching.py
@@ -2,29 +2,9 @@ from joblib import load
 count_vect = load('count_vect.joblib')
 tfidf_transformer = load('tfidf_transformer.joblib')
 clf = load('classifier.joblib')
-import csv
-import random
-import numpy as np
+
 from sklearn.metrics.pairwise import cosine_similarity
 
-
-qn_q={}
-q_a = {}
-questions = []
-intents =[] 
-with open("./QA＿intent.csv", 'r',encoding='utf-8') as file:
-    csvreader = csv.reader(file)
-    for row in csvreader:
-        #store QuestionNumber-Question, Question-Answer
-        qn_q[row[0]] = row[1]
-        if row[1] in q_a:
-            q_a[row[1]].append(row[2])
-        else:
-            q_a[row[1]]=[row[2]]
-        
-        questions.append(row[1])
-        intents.append(row[0])
-        
 
 def answer(msg):
     new_data = [msg]
