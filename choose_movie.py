f98bcce355bbc1b1d7fa10f3fbecd2e2f88b7fec
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-12-01 14:21:55 +0000

    try except condition change

--- choose_movie.py
+++ choose_movie.py
@@ -12,16 +12,16 @@ def ask_which_movie(user):
             print("Seems like you haven't choose a movie to watch")
             user_ans= input(f"What do you want to watch? (within {list(range(1,len(movie_list)+1))}):")
             try:
-                if len(user_ans) > 1:
-                    print(f"Sorry, we can't arrange with your answer.")
-                    decide_yet = input("Have you decided which movie? (Y/N): ")
-                    if 'n' in decide_yet.lower():
-                        leave = True
-                elif int(user_ans) in list(range(1,len(movie_list)+1)):
+                if int(user_ans) in list(range(1,len(movie_list)+1)):
                     interested_movie_index = int(user_ans)-1
                     user_movie = movie_list[interested_movie_index].lower()
                     user.target_movie(user_movie)
                     print(f"OK, we will choose {user_movie.upper()} for you")
+                else:
+                    print(f"Sorry, we can't arrange with your answer.")
+                    decide_yet = input("Have you decided which movie? (Y/N): ")
+                    if 'n' in decide_yet.lower():
+                        leave = True
             except:
                 print(f"Sorry you must type in an number within {list(range(1,len(movie_list)+1))}")
                 decide_yet = input("Have you decided which movie? (Y/N): ")
