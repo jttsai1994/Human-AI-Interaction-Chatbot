914c21b2473e148b7b264733da2df753657f222c
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-12-01 15:07:19 +0000

    let user ask how to book

---
+++ ask_booking.py
@@ -0,0 +1,18 @@
+import re
+import booking_process
+import random
+from params import responses_movie_list
+import time
+def start_booking(user):
+    print("I can help you to start/finish your booking")
+    time(1)
+    print("All you have to do is decide: Movie/ Visit Date/ Visit Time/ Payment Method")
+    time(1)
+    print("Seems like you have not made any movie ticket reservation")
+    time(1)
+    print(random.choice(responses_movie_list))
+    _ans = input("Would you like to book a movie ticket right away? (Y/N):")
+    if re.search('y*',_ans.lower())[0]:
+        booking_process.booking(user)
+    else:
+        print("You still have time to make a decision! No worries!") 
