b45d92479deb891ad584bfd9fafce00ccbb0a0ac
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-12-02 00:20:06 +0000

    create change_booking to reuse this function

---
+++ change_booking.py
@@ -0,0 +1,19 @@
+import re
+import booking_process
+import random
+
+def lets_change_booking(user):
+    print('Which details you want to change? Movie, Date, or Time? ')
+    _change =input('Please type 1 for Movie, 2 for Date, 3 for Time: ')
+    if _change == '1':
+        user.reset_all_booking_details()
+        booking_process.booking(user)
+    elif _change == '2':
+        user.reset_visitDate()
+        user.reset_visitTime()
+        booking_process.booking(user)
+    elif _change == '3':           
+        user.reset_visitTime()
+        booking_process.booking(user)
+    else:
+        print('sorry, because of the wrong input, you could not change booking details in this turn.')
