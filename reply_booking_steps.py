767eac84b6e88c5b76974358bf800d119cfa4a0e
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-12-03 21:52:33 +0000

    rename as reply_booking_steps and move the code for asking user if start booking to ask_start_booking.py

---
+++ reply_booking_steps.py
@@ -0,0 +1,9 @@
+from params import responses_movie_list
+import time
+def reply():
+    print("I can help you to start/finish your booking")
+    time.sleep(1)
+    print("All you have to do is decide: Movie/ Visit Date/ Visit Time/ Payment Method")
+    time.sleep(1)
+
+     
