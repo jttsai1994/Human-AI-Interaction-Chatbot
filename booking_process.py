897ba3c2a1671a18523ea3eccc93d4d7d9af8544
jttsai1994 <60954028+jttsai1994@users.noreply.github.com>
2023-12-04 23:11:33 +0000

    if user already finished booking and ask for booking-> confirm his details

--- booking_process.py
+++ booking_process.py
@@ -5,6 +5,8 @@ import choose_movie_time
 import choose_paymethod
 import time
 import confirm_movie_detail
+import check_personal_confirmation as p_confirm
+
 def booking(user):
     movie_pattern = "|".join(params.movie_list).lower()
     stop = False
@@ -20,7 +22,9 @@ def booking(user):
             print(f"Congrats! You just finished your reservation for watching {user.movie.upper()}")
             time.sleep(1)
             print(f"Looking forward to seeing you on {user.visitDate}/ {user.visitTime}!")
-
+            break
+    # if user already make a booking
+    p_confirm.booked_details(user)            
         
         # response = match_rules.send_message(user_input)
         
